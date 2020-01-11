c1eb9fe8a9c5cf28491664a8261d468185634aca
Elena Dieva <Ledivia@yandex.ru>
2020-01-11 18:57:52 +0300

    добавлена строка username

--- Lesson2/lesson2_task3.py
+++ Lesson2/lesson2_task3.py
@@ -12,5 +12,6 @@ def driver(request):
 
 def test_example(driver):
     driver.get("http://localhost/litecart/admin/login.php")
+    driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
